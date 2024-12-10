4616147a5ba6c92b7694e3727fdb98ce87f680aa
dayalan <daya.22.thilak@gmail.com>
2024-12-10 19:41:07 +0530

    manual consumer offset commit management code added

--- src/main/java/com/orderapp/configuration/ProducerEventsKafkaConsumerConfiguration.java
+++ src/main/java/com/orderapp/configuration/ProducerEventsKafkaConsumerConfiguration.java
@@ -1,11 +1,36 @@
 package com.orderapp.configuration;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 
 @Configuration
 @EnableKafka
 @Slf4j
 public class ProducerEventsKafkaConsumerConfiguration {
+
+    private KafkaProperties properties;
+
+    public ProducerEventsKafkaConsumerConfiguration(KafkaProperties properties){
+        this.properties = properties;
+    }
+
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(ConcurrentKafkaListenerContainerFactoryConfigurer configurer, ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory();
+        configurer.configure(factory, (ConsumerFactory)kafkaConsumerFactory.getIfAvailable(() -> {
+            return new DefaultKafkaConsumerFactory(this.properties.buildConsumerProperties());
+        }));
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        return factory;
+    }
 }
